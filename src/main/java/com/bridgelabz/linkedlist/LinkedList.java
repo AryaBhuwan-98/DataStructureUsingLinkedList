208ed7d684551043a6fc6f0375d07f763dc5d273
AryaBhuwan-98 <aryabhuwan873@gmail.com>
2022-11-27 22:51:43 -0800

    Adding program to insert element at particular position in linkedlist

--- src/main/java/com/bridgelabz/linkedlist/LinkedList.java
+++ src/main/java/com/bridgelabz/linkedlist/LinkedList.java
@@ -1,8 +1,8 @@
 package com.bridgelabz.linkedlist;
 
 public class LinkedList {
-Node head, tail;
-	
+	private Object tail;
+
 	public Node push(int data) {
 		Node newNode = new Node(data);
 		if (head == null) {
@@ -17,7 +17,9 @@ Node head, tail;
 	}
 	public void append(int data) { // 56=> 30=> 70
         Node newNode = new Node(data);
-        if (head == null) {
+        Object head;
+		Node tail;
+		if (head == null) {
             head = newNode;
             tail = newNode;
         } else {
@@ -52,12 +54,13 @@ Node head, tail;
     }
 	
 	public void popLast(){
-        if (head == null)
+        Object head;
+		if (head == null)
             System.out.println("No elements to delete..");
         else if (head.next == null)
             head = null;
         else {
-            Node temp = head;
+            Node temp = (Node) head;
 
             while (temp.next.next != null) {
                 temp = temp.next;
@@ -68,11 +71,12 @@ Node head, tail;
 }
 	public void search(int searchData)
     {
-        if(head.key == searchData)
+        Object head;
+		if(head.key == searchData)
             System.out.println(searchData + " is found in LinkedList");
         else
         {
-            Node temp=head;
+            Object temp=head;
 
             boolean isFound=false;
 
@@ -92,5 +96,56 @@ Node head, tail;
             else
                 System.out.println(searchData+"is not found in LinkedList");
         }
+}
+	public int size() {
+        int count = 0; // No data 1 element
+
+        Node temp = head;
+
+        while (temp != null) {
+            count++;
+            temp = temp.next;
+        }
+
+        return count;
+    }
+
+    public void insertAtSpecificIndex(int userIndex, int data) {
+
+        // User trying to insert @ first position
+        if (userIndex == 0)
+            push(data);
+
+            // // User trying to insert @ last position
+        else if (userIndex == size())
+           append(data);
+
+            // User trying to insert @ invalid position
+        else if (userIndex < 0 || userIndex >= size())
+            System.out.println("Invalid index");
+            // User trying to insert @ specific index
+        else {
+            // Creating New Node
+            Node newNode = new Node(data);
+
+            // To track traversing
+            int index = 0;
+
+            // Pointers to track left & right side elements
+            Node left = head;
+            Node right = left.next;
+
+            // Traverse or Move till last element before user entered index
+            while (index < userIndex - 1) {
+                left = left.next;
+                right = right.next;
+                index++;
+            }
+
+            // Connecting new Node with right side elements
+            newNode.next = right;
+            // Connecting left side elements with new Node
+            left.next = newNode;
+}
 }
 }
